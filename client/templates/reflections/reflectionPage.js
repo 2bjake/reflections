24f1cef1e033c472b35cf7df898e9ef9a7d8405d
2bjake <fosterjake@gmail.com>
2014-12-12 10:42:32 -0800

    cut out post stuff, switch to less, make ratings colorful

--- client/templates/reflections/reflectionPage.js
+++ client/templates/reflections/reflectionPage.js
@@ -1,5 +1,16 @@
+
+var colors = ['#E50016', '#E01C00', '#DC4D00', '#D87D00', '#D4AA00', '#C9CF00', '#97CB00', '#66C700', '#38C300', '#0CBF00'];
+
 Template.reflectionPage.helpers({
   formattedDate: function() {
     return dateUtils.fromDbToDisplayFormat(this.date);
+  },
+
+  enjoyColor: function() {
+    return colors[this.enjoy];
+  },
+
+  effectiveColor: function() {
+    return colors[this.effective];
   }
 });
--- server/fixtures.js
+++ server/fixtures.js
@@ -1,20 +1,4 @@
-if (Posts.find().count() === 0) {
-  Posts.insert({
-    title: 'Introducing Telescope',
-    url: 'http://sachagreif.com/introducing-telescope/'
-  });
-
-  Posts.insert({
-    title: 'Meteor',
-    url: 'http://meteor.com'
-  });
-
-  Posts.insert({
-    title: 'The Meteor Book',
-    url: 'http://themeteorbook.com'
-  });
-}
-
+//TODO: kill this? Else fill it out. Also, make it only run in test
 if (Reflections.find().count() === 0) {
   // Jake
   Reflections.insert({
