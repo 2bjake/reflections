e04c72cb08f72060ff52ecae523cf417d94e577c
2bjake <fosterjake@gmail.com>
2015-01-08 15:35:31 -0800

    add radios and select, add wlc form

--- client/templates/reflections/reflectionCreate.js
+++ client/templates/reflections/reflectionCreate.js
@@ -31,21 +31,29 @@ Template.reflectionCreate.events({
   }
 });
 
+wlcFields = [
+        {id:"foodPoints", label:"Food Points", type:"select", values:[5,4,3,2,1,0]},
+        {id:"exercise", label:"Did you exercise?", type:"radios", values:["yes", "no"]},
+        {id:"stretch", label:"Did you stretch?", type:"radios", values:["yes", "no"]},
+        {id:"challenge", label:"Did you do the weekly challenge today?", type:"radios", values:["yes", "no"]},
+        {id:"reflection", label:"Reflection", type:"textarea", attributes:{rows: "5"}}
+      ];
+
+workFields = [
+        {id:"title", label:"Title", type:"text"},
+        {id:"journal", label:"Journal entry", type:"textarea", attributes:{rows: "20"}},
+        {id:"accomplishments", label:"Accomplishments", type:"textarea", attributes:{rows: "5"}},
+        {id:"todos", label:"Things to do tomorrow", type:"textarea", attributes:{rows: "5"}},
+        {id:"enjoy", label:"How enjoyable was today?", type:"select", values:[10,9,8,7,6,5,4,3,2,1]},
+        {id:"effective", label:"How effective were you today?", type:"select", values:[10,9,8,7,6,5,4,3,2,1]}
+      ];
+
 Template.reflectionCreate.helpers({
   today: function() {
     return dateUtils.todayDisplayFormat();
   },
 
   formData: function() {
-    return {
-      fields: [
-        {id:"title", label:"Title", type:"text"},
-        {id:"journal", label:"Journal entry", type:"textarea", attributes:{rows: "20"}},
-        {id:"accomplishments", label:"Accomplishments", type:"textarea", attributes:{rows: "5"}},
-        {id:"todos", label:"Things to do tomorrow", type:"textarea", attributes:{rows: "5"}},
-        {id:"enjoy", label:"How enjoyable was today? (1 - 10)", type:"number", attributes:{min:"1", max: "10"}},
-        {id:"effective", label:"How effective were you today? (1 - 10)", type:"number", attributes:{min:"1", max: "10"}}
-      ]
-    };
+    return {fields: workFields};
   }
 });
